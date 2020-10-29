738aec730b8023e9a677b97a5369aee3a8391ac2
atiii <aty.sokhansanj@gmail.com>
2020-10-29 15:05:04 +0330

    8/8/99

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -7,11 +7,62 @@ import "./../css/Login.css";
 import logo from "./../img/footer_logo.png";
 
 //import reactstrap
-import { Button, Form, FormGroup, Input, FormText } from "reactstrap";
+import { Button, Form, FormGroup, Input } from "reactstrap";
 
+//import packages
+import ReCAPTCHA from "react-google-recaptcha";
 import { NavLink } from "react-router-dom";
+import SimpleReactValidator from "simple-react-validator";
+
+//import utils
+import { successMessage , errorMessage} from './../utils/message'
 
 export class Login extends Component {
+
+  state = {
+    username: "",
+    password: "",
+    captcha:"",
+  }
+
+  constructor() {
+    super()
+    this.validator = new SimpleReactValidator({
+       messages: {
+        username: 'لطفا نام کاربری  خود را وارد کنید',
+        password:'لطفا کلمه عبور را وارد کنید',
+    // default: 'لطفا فرم را به درستی تکمیل کنید' ,
+  },
+    });
+  }
+  onChange = (value) => {
+    console.log("Captcha value:", value);
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+     if (this.validator.allValid()) {
+       successMessage("ورود شما با موفقیت انجام شد")
+       console.info("SUBMITED")
+       this.resetForm();
+     } else {
+       errorMessage("خطایی رخ داده است!")
+       console.error("UNSUBMITED");
+       this.validator.showMessages();
+       this.forceUpdate();
+     }
+  };
+
+  resetForm = () => {
+    this.setState({ username: "", password: "", captcha: "" });
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+     this.setState({ [name]: value });
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -24,32 +75,58 @@ export class Login extends Component {
               <NavLink className="register" to="/Signup">
                 <Button className="signup-btn" type="button">
                   ثبت نام
-              </Button>
-                </NavLink>
+                </Button>
+              </NavLink>
             </div>
           </section>
           <section className="section-left">
             <img src={logo} alt="" className="logo" />
-            <Form className="login-form">
+            <Form className="login-form" onSubmit={(e) => this.handleSubmit(e)}>
               <FormGroup>
                 <Input
                   type="text"
                   name="username"
                   id="username"
                   placeholder="نام کاربری (کد ملی)"
+                  value={this.state.username}
+                  onChange={(e) => this.handleChange(e)}
                 />
+                {this.validator.message(
+                  "نام کابری وارد شده معتبر نمی باشد",
+                  this.state.username,
+                  "required|integer"
+                )}
                 <Input
                   type="password"
                   name="password"
                   id="password"
                   placeholder="کلمه عبور"
+                  value={this.state.password}
+                  onChange={(e) => this.handleChange(e)}
                 />
+                {this.validator.message(
+                  "وارد کردن کلمه عبور اجباریست",
+                  this.state.password,
+                  "required|alpha"
+                )}
                 <Input
-                  type="password"
-                  name="confirmPassword"
-                  id="confirmPassword"
-                  placeholder="تکرار کلمه عبور"
+                  type="text"
+                  name="captcha"
+                  id="captcha"
+                  placeholder="کد امنیتی"
+                  value={this.state.captcha}
+                  onChange={(e) => this.handleChange(e)}
                 />
+                {this.validator.message(
+                  "لطفا کد امنیتی را وارد کنید",
+                  this.state.captcha,
+                  "required|alpha_num"
+                )}
+                {/* <ReCAPTCHA
+                  sitekey="Your client site key"
+                  size="invisible"
+                  onChange={() => this.onChange}
+                /> */}
               </FormGroup>
               <div className="button-section">
                 <NavLink to="/" className="forget">
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -7,11 +7,17 @@ import "./../css/Signup.css";
 import logo from "./../img/footer_logo.png";
 
 //import reactstrap
-import { Button, Form, FormGroup, Input, FormText } from "reactstrap";
+import { Button, Form, FormGroup, Input } from "reactstrap";
 
 import { NavLink } from "react-router-dom";
 
 export class Signup extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  handleChange = (e) => {
+    e.preventDefault();
+  };
   render() {
     return (
       <React.Fragment>
@@ -25,13 +31,26 @@ export class Signup extends Component {
                   name="username"
                   id="username"
                   placeholder="نام کاربری (کد ملی)"
+                  onChange={(e)=>this.handleChange(e)}
+                />
+                <Input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="کلمه عبور"
+                   onChange={(e)=>this.handleChange(e)}
+                />
+                <Input
+                  type="confirmPassword"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  placeholder="تکرار کلمه عبور"
+                   onChange={(e)=>this.handleChange(e)}
                 />
               </FormGroup>
               <div className="button-section">
-                <NavLink to="/" className="forget">
-                  
-                </NavLink>
-                <Button className="login-btn" type="submit">
+                <NavLink to="/" className="forget"></NavLink>
+                <Button className="register" type="submit">
                   ثبت نام
                 </Button>
               </div>
@@ -44,7 +63,7 @@ export class Signup extends Component {
                 قبلا ثبت نام کرده اید ؟
               </NavLink>
               <NavLink className="login-link" to="/">
-                <Button className="login-btn" type="button">
+                <Button className="login-button" type="button">
                   ورود
                 </Button>
               </NavLink>
